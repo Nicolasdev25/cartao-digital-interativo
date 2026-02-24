ef05a2d2c192755d30709a8e72fbcfd8485eb2f2
Nicolasdev25 <nicolasleao.dev@gmail.com>
2026-02-24 12:20:19 -0300

    Update script.js
    add comenta.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// 1. Selecionamos os elementos que você criou no HTML
+
 const inputNome = document.getElementById('input-name');
 const displayNome = document.getElementById('display-name');
 
-// 2. Definimos um nome padrão para o cartão não começar vazio
 displayNome.innerText = "Seu Nome Aqui";
 
-// 3. Adicionamos o "ouvinte" para detectar quando você digita
+
 inputNome.addEventListener('input', () => {
     
     // Se o input não estiver vazio, coloca o que foi digitado no h1
